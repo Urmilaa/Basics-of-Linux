ea133f28dc4fa55d70dc1d4e588b6d49d92cfa00
Urmilaa <urmikher@gmail.com>
2026-03-03 16:01:46 +0530

    Create Memory_check.sh for RAM monitoring
    Add a script to check available RAM and display warnings if low.

---
+++ 06. Bash Scripting /Memory_check.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+set -x # set debug mode
+set -e # exit if any error
+set -o # pipefail
+
+FREE_SPACE=$(free -mt | grep "Total" | awk '{print $4}')
+
+TH=3000
+
+if [[ $FREE_SPACE -lt $TH ]]
+then
+        echo "WARNING , RAM is running low"
+        else
+                echo "RAM Space is sufficient $FREE_SPACE M "
+                fi
+
+
+
+df -h
+
+ewrwerwerwer
+
+free -g
